2da105ebecfcaead732d96668ed9042e22f96565
GuiRomanholi <guilhermeroma343@gmail.com>
2024-05-22 19:25:24 -0300

    commit rapido

--- src/test/TesteCarro.java
+++ src/test/TesteCarro.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 public class TesteCarro {
     public static void main(String[] args) {
+        Scanner leitorNum = new Scanner(System.in);
         System.out.println("-------------------------------------------------------");
         Cliente cliente = new Cliente();
         cliente.setNome("Vitor");
@@ -17,64 +18,143 @@ public class TesteCarro {
 
         System.out.println(cliente.exibirCliente("Vitor","355.233.654-12"));
 
-        System.out.println("-------------------------------------------------------");
+        System.out.println("-------------------------------------------------------\n");
+        boolean prosseguir = true;
+        int continuar;
+        while (prosseguir == true){
+            System.out.println("Deseja ver os detalhes do Carro? (Sim = 1) e (Não = 2)");
+            continuar = leitorNum.nextInt();
+            if (continuar == 1){
+                Carro carro = new Carro();
+                carro.setPlaca("1FD3R4");
+                carro.setCor("Prata");
+                carro.setMarca("Fiat");
+                carro.setModelo("CRONOS");
 
-        Carro carro = new Carro();
-        carro.setPlaca("1FD3R4");
-        carro.setCor("Prata");
-        carro.setMarca("Fiat");
-        carro.setModelo("CRONOS");
+                System.out.println(carro.exibirDetalhes());
+                break;
 
+            } else if (continuar == 2) {
+                System.out.println("Prosseguindo...");
+                break;
+            }else {
+                System.out.println("Por favor informe 1 para Sim e 2 para Não");
+            }
+        }
 
-        System.out.println(carro.exibirDetalhes());
+        System.out.println("-------------------------------------------------------\n");
 
-        System.out.println("-------------------------------------------------------");
+        while (prosseguir == true){
+            System.out.println("Deseja ver os detalhes do Guincho? (Sim = 1) e (Não = 2)");
+            continuar = leitorNum.nextInt();
+            if (continuar == 1){
+                Guincho guincho = new Guincho();
+                guincho.setMarca("Ford");
+                guincho.setPlaca("3FGU76");
+                guincho.setAno_fabricacao("2006");
 
-        Guincho guincho = new Guincho();
-        guincho.setMarca("Ford");
-        guincho.setPlaca("3FGU76");
-        guincho.setAno_fabricacao("2006");
+                System.out.println(guincho.exibirDetalhes());
+                guincho.acionarGuincho();
+                break;
 
-        System.out.println(guincho.exibirDetalhes());
-        guincho.acionarGuincho();
-
-        System.out.println("-------------------------------------------------------");
-
-        Diagnostico diagnostico = new Diagnostico();
-        diagnostico.exibirDiagnostico();
-        diagnostico.mostrarDescricao();
-
-
-        System.out.println("-------------------------------------------------------");
-
-        Endereco endereco = new Endereco();
-        endereco.setCep("08480040");
-        endereco.setCidade("São Paulo");
-        endereco.setEstado("São Paulo");
-        endereco.setNumero("223");
-
-        System.out.println(endereco.exibirEndereco());
-
-        System.out.println("-------------------------------------------------------");
-
-        System.out.println("DADOS DO PEDIDO");
-        Pedido pedido = new Pedido();
-        pedido.setData(LocalDate.ofEpochDay(12/10/2024));
-        pedido.setHora("10:10");
-        pedido.setNumero("13642");
-
-        System.out.println(pedido.dadosPedido());
-
-        System.out.println("-------------------------------------------------------");
-
-        FrmPagamento frmPagamento = new FrmPagamento();
-        frmPagamento.setId("4235113");
-        frmPagamento.setStatus("Aprovado");
-        frmPagamento.escolherForma();
-        frmPagamento.exibirValor(200);
-
-        System.out.println("-------------------------------------------------------");
+            } else if (continuar == 2) {
+                System.out.println("Prosseguindo...");
+                break;
+            }else {
+                System.out.println("Por favor informe 1 para Sim e 2 para Não");
+            }
+        }
 
 
+        System.out.println("-------------------------------------------------------\n");
+
+        while (prosseguir == true){
+            System.out.println("Deseja tentar resolver o problema? (Sim = 1) e (Não = 2)");
+            continuar = leitorNum.nextInt();
+            if (continuar == 1){
+                Diagnostico diagnostico = new Diagnostico();
+                diagnostico.exibirDiagnostico();
+                diagnostico.mostrarDescricao();
+                break;
+
+            } else if (continuar == 2) {
+                System.out.println("Prosseguindo...");
+                break;
+            }else {
+                System.out.println("Por favor informe 1 para Sim e 2 para Não");
+            }
+        }
+
+        System.out.println("-------------------------------------------------------\n");
+
+        while (prosseguir == true){
+            System.out.println("Deseja ver detalhes do Endereço? (Sim = 1) e (Não = 2)");
+            continuar = leitorNum.nextInt();
+            if (continuar == 1){
+                Endereco endereco = new Endereco();
+                endereco.setCep("08480040");
+                endereco.setCidade("São Paulo");
+                endereco.setEstado("São Paulo");
+                endereco.setNumero("223");
+
+                System.out.println(endereco.exibirEndereco());
+                break;
+            } else if (continuar == 2) {
+                System.out.println("Prosseguindo...");
+                break;
+            }else {
+                System.out.println("Por favor informe 1 para Sim e 2 para Não");
+            }
+        }
+
+
+
+        System.out.println("-------------------------------------------------------\n");
+
+        while (prosseguir == true){
+            System.out.println("Deseja ver os dados do pedido? (Sim = 1) e (Não = 2)");
+            continuar = leitorNum.nextInt();
+            if (continuar == 1){
+                System.out.println("DADOS DO PEDIDO");
+                Pedido pedido = new Pedido();
+                pedido.setData(LocalDate.ofEpochDay(12/10/2024));
+                pedido.setHora("10:10");
+                pedido.setNumero("13642");
+
+                System.out.println(pedido.dadosPedido());
+                break;
+            } else if (continuar == 2) {
+                System.out.println("Prosseguindo...");
+                break;
+            }else {
+                System.out.println("Por favor informe 1 para Sim e 2 para Não");
+            }
+        }
+
+
+        System.out.println("-------------------------------------------------------\n");
+
+        while (prosseguir == true){
+            System.out.println("Deseja escolher a forma de pagamento? (Sim = 1) e (Não = 2)");
+            continuar = leitorNum.nextInt();
+            if (continuar == 1){
+                FrmPagamento frmPagamento = new FrmPagamento();
+                frmPagamento.setId("4235113");
+                frmPagamento.setStatus("Aprovado");
+                frmPagamento.escolherForma();
+                frmPagamento.exibirValor(200);
+                break;
+            } else if (continuar == 2) {
+                System.out.println("Prosseguindo...");
+                break;
+            }else {
+                System.out.println("Por favor informe 1 para Sim e 2 para Não");
+            }
+        }
+
+        System.out.println("-------------------------------------------------------\n");
+
+        
     }
+
 }
